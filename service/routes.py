80463553438f68e464b9d707071b33976e028ae9
migueel-st <ma.samaniego@hotmail.com>
2025-03-30 02:30:57 -0400

    Add route to list accounts

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,13 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    app.logger.info(f"Request to list Accounts")
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info(f"Found {len(account_list)} account(s)")
+    return jsonify(account_list), status.HTTP_200_OK
 
 
 ######################################################################
